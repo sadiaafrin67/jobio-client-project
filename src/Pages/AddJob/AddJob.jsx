6ff9bd129e7c943b8cf3e8ddc088354cf7f779b4
sadiaafrin67 <sa540276@gmail.com>
2023-11-05 15:05:57 +0600

    add my register section

--- src/Pages/AddJob/AddJob.jsx
+++ src/Pages/AddJob/AddJob.jsx
@@ -1,50 +1,57 @@
 // import Swal from 'sweetalert2'
 
 const AddJob = () => {
+  const handleAddProduct = (e) => {
+    e.preventDefault();
 
-    const handleAddProduct = (e) => {
-        e.preventDefault()
+    const form = e.target;
+    const name = form.name.value;
+    const price = form.price.value;
+    const brand = form.brand.value;
+    const description = form.description.value;
+    const rating = form.rating.value;
+    const category = form.category.value;
+    const image = form.image.value;
 
-        const form = e.target;
-        const name = form.name.value;
-        const price = form.price.value;
-        const brand = form.brand.value;
-        const description = form.description.value;
-        const rating = form.rating.value;
-        const category = form.category.value;
-        const image = form.image.value;
-        
+    const newProduct = {
+      name,
+      price,
+      brand,
+      description,
+      rating,
+      category,
+      image,
+    };
+    console.log(newProduct);
 
-        const newProduct = { name, price, brand, description, rating, category, image };
-        console.log(newProduct);
+    // fetch('https://touchup-brand-server.vercel.app/products', {
+    //     method: 'POST',
+    //     headers: {
+    //         'content-type': 'application/json'
+    //     },
+    //     body: JSON.stringify(newProduct)
+    // })
+    //     .then(res => res.json())
+    //     .then(data => {
+    //         console.log(data);
 
-        // fetch('https://touchup-brand-server.vercel.app/products', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(newProduct)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data);
+    //         if(data.insertedId){
+    //             Swal.fire({
+    //                 title: 'Success!',
+    //                 text: 'Product Added Successfully',
+    //                 icon: 'success',
+    //                 confirmButtonText: 'Ok'
+    //               })
+    //         }
+    //     })
+  };
 
-
-        //         if(data.insertedId){
-        //             Swal.fire({
-        //                 title: 'Success!',
-        //                 text: 'Product Added Successfully',
-        //                 icon: 'success',
-        //                 confirmButtonText: 'Ok'
-        //               })
-        //         }
-        //     })
-    }
-
-    return (
-        <div>
+  return (
+    <div>
       <div className="bg-[#f1f3f5] rounded-xl md:p-24 p-4 my-20">
-        <h2 className="text-3xl mb-10 text-center font-extrabold">Add Your <span className="text-[#1e3c72]">Cosmetics</span></h2>
+        <h2 className="text-3xl mb-10 text-center font-extrabold">
+          Add Your <span className="text-[#1e3c72]">Cosmetics</span>
+        </h2>
         <form onSubmit={handleAddProduct}>
           {/* form name and price row */}
           <div className="md:flex gap-4 mb-8">
@@ -140,18 +147,24 @@ const AddJob = () => {
           </div>
 
           {/* from image URL row */}
-          <div className="mb-8">
-            <div className="form-control w-full">
+
+          <div className="mb-8 ">
+            <div className="form-control w-full  ">
               <label className="label">
-                <span className="label-text">Image</span>
+                <span className="label-text">Category</span>
               </label>
-              <label className="input-group">
-                <input
-                  type="text"
-                  placeholder="Image URL"
-                  name="image"
-                  className="input form-border  input-bordered w-full"
-                />
+              <label className="">
+                <select
+                  name="category"
+                  className="select select-bordered w-full"
+                >
+                  <option value="" disabled selected>
+                    Select a category
+                  </option>
+                  <option value="website">Web Development</option>
+                  <option value="web design"> Graphics Design</option>
+                  <option value="digital marketing">Digital Marketing</option>
+                </select>
               </label>
             </div>
           </div>
@@ -164,7 +177,7 @@ const AddJob = () => {
         </form>
       </div>
     </div>
-    );
+  );
 };
 
 export default AddJob;
