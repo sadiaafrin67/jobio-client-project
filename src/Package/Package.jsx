2df9776c9ce1906eee248edcb49969a0c051300f
sadiaafrin67 <sa540276@gmail.com>
2023-11-08 04:19:59 +0600

    add my pacakge section

---
+++ src/Package/Package.jsx
@@ -0,0 +1,424 @@
+const Package = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="text-3xl text-center font-bold text-black">
+        Great Price{" "}
+        <span className="text-[#1e3c72] text-4xl mb-3">For You</span>
+      </h1>
+      <p className="text-center font-medium text-base mb-9">
+        Choose your package. We have a package for you. Choose the one that is
+        right for you.
+      </p>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <div className="relative flex w-full  flex-col rounded-xl bg-gradient-to-tr from-blue-950 to-blue-400 bg-clip-border p-8 text-white shadow-md shadow-blue-500/40">
+          <div className="relative pb-8 m-0 mb-8 overflow-hidden text-center text-gray-700 bg-transparent border-b rounded-none shadow-none border-white/10 bg-clip-border">
+            <p className="block font-sans text-2xl antialiased font-bold leading-normal text-white uppercase">
+            Basic
+            </p>
+            <p className="text-center font-normal text-sm text-white mt-4">For organising every corner of your life</p>
+            <h1 className="flex justify-center gap-1 mt-6 font-sans antialiased font-normal tracking-normal text-white text-7xl">
+              <span className="mt-2 text-4xl">$</span>29
+              <span className="self-end text-4xl">/Per Month</span>
+            </h1>
+          
+          </div>
+          <div className="p-0">
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Access Whole Items
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Sync across device
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Share with 10 guests
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  1 year free updates
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  Life time technical support
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="p-0 mt-12">
+            <button
+              className="block w-full select-none rounded-lg bg-white py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-blue-800 shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-dark="true"
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+        <div className="relative flex w-full  flex-col rounded-xl bg-gradient-to-tr from-blue-400 to-blue-950 bg-clip-border p-8 text-white shadow-md shadow-blue-500/40">
+          <div className="relative pb-8 m-0 mb-8 overflow-hidden text-center text-gray-700 bg-transparent border-b rounded-none shadow-none border-white/10 bg-clip-border">
+            <p className="block font-sans text-2xl antialiased font-bold leading-normal text-white uppercase">
+            Professional
+            </p>
+            <p className="text-center font-normal text-sm text-white mt-4">For organising every corner of your life</p>
+            <h1 className="flex justify-center gap-1 mt-6 font-sans antialiased font-normal tracking-normal text-white text-7xl">
+              <span className="mt-2 text-4xl">$</span>99
+              <span className="self-end text-4xl">/6 Months</span>
+            </h1>
+          </div>
+          <div className="p-0">
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Access Whole Items
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Sync across device
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Share with 15 guests
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  2 year free updates
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  Life time technical support
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="p-0 mt-12">
+            <button
+              className="block w-full select-none rounded-lg bg-white py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-blue-800 shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-dark="true"
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+        <div className="relative flex w-full  flex-col rounded-xl bg-gradient-to-tr from-blue-950 to-blue-400 bg-clip-border p-8 text-white shadow-md shadow-blue-500/40">
+          <div className="relative pb-8 m-0 mb-8 overflow-hidden text-center text-gray-700 bg-transparent border-b rounded-none shadow-none border-white/10 bg-clip-border">
+            <p className="block font-sans text-2xl antialiased font-bold leading-normal text-white uppercase">
+            VIP Package
+            </p>
+            <p className="text-center font-normal text-sm text-white mt-4">For organising every corner of your life</p>
+            <h1 className="flex justify-center gap-1 mt-6 font-sans antialiased font-normal tracking-normal text-white text-7xl">
+              <span className="mt-2 text-4xl">$</span>200
+              <span className="self-end text-4xl">/1 Year</span>
+            </h1>
+          </div>
+          <div className="p-0">
+            <ul className="flex flex-col gap-4">
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Access Whole Items
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Sync across device
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                Share with 20 guests
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  2.5 year free updates
+                </p>
+              </li>
+              <li className="flex items-center gap-4">
+                <span className="p-1 border rounded-full border-white/20 bg-white/20">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-3 h-3"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
+                    />
+                  </svg>
+                </span>
+                <p className="block font-sans text-base antialiased font-normal leading-relaxed text-inherit">
+                  Life time technical support
+                </p>
+              </li>
+            </ul>
+          </div>
+          <div className="p-0 mt-12">
+            <button
+              className="block w-full select-none rounded-lg bg-white py-3.5 px-7 text-center align-middle font-sans text-sm font-bold uppercase text-blue-800 shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-dark="true"
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Package;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Banner from './Banner/Banner';
 // import BannerShare from "../../Shared/BannerShare";
 import JobCat from '../../Pages/Home/Job/JobCat';
 import Sopnsor from '../../Shared/Sopnsor';
+import Package from '../../Package/Package';
 
 
 
@@ -14,6 +15,7 @@ const Home = () => {
          <JobCat></JobCat>
           {/* <BannerShare></BannerShare> */}
           <Sopnsor></Sopnsor>
+          <Package></Package>
         </div>
     );
 };
