e2c4faa9bbd41a86985a6e8ea4f524f30cd99c26
sadiaafrin67 <sa540276@gmail.com>
2023-11-08 23:49:23 +0600

    add my eve file

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -4,14 +4,20 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCBO_wNhIdanG1S_cM7cCGjnAppblxVDgc",
-  authDomain: "jobio-client-project.firebaseapp.com",
-  projectId: "jobio-client-project",
-  storageBucket: "jobio-client-project.appspot.com",
-  messagingSenderId: "437801382780",
-  appId: "1:437801382780:web:5242ec76bd1216416ceaf9"
-};
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId,
+  };
+
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
